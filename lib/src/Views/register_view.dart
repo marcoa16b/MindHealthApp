2151e6e8a516a4bc19bf5877870a925d1a2f04a6
KenniaM <107071119+KenniaM@users.noreply.github.com>
2024-07-06 23:53:55 -0600

    Register

--- lib/src/Views/register_view.dart
+++ lib/src/Views/register_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -30,10 +29,12 @@ class _RegisterPageState extends State<RegisterPage> {
         final user = await _dbService.registerWithEmail(username, password);
         if (user != null) {
           // Si el registro es exitoso, redirigir al usuario a la página de inicio de sesión
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
           print('Registration successful');
         } else {
-          _showErrorDialog('Failed to register. Please check your details and try again.');
+          _showErrorDialog(
+              'Failed to register. Please check your details and try again.');
         }
       } catch (e) {
         _showErrorDialog('An error occurred. Please try again later.');
@@ -66,126 +67,131 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
+  String? _emailValidator(String? value) {
+    const emailPattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$';
+    final regExp = RegExp(emailPattern);
+
+    if (value == null || value.isEmpty) {
+      return "Enter an email";
+    } else if (!regExp.hasMatch(value)) {
+      return "Enter a valid email";
+    }
+    return null;
+  }
+
+  String? _passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Enter a password";
+    } else if (value.length < 6) {
+      return "Password must be at least 6 characters";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
-        statusBarBrightness: Brightness.dark
-    ));
+        statusBarBrightness: Brightness.dark));
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(232, 236, 244, 1),
-        toolbarHeight: 0,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/register.png',
-              width: 430,
-              height: 473,
-              fit: BoxFit.cover
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
-                child: Transform.translate(
-                  offset: Offset(0, -30),
-                  child: Text(
-                    "Register.",
-                    style: GoogleFonts.indieFlower(
-                      fontSize: 62,
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(232, 236, 244, 1),
+          toolbarHeight: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/register.png',
+                  width: 430, height: 473, fit: BoxFit.cover),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+                  child: Transform.translate(
+                    offset: Offset(0, -30),
+                    child: Text(
+                      "Register.",
+                      style: GoogleFonts.indieFlower(
+                        fontSize: 62,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Transform.translate(
-              offset: Offset(0, -30),
-              child: Form(
-                key: _formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
-                      child: TextFormField(
-                        controller: _usernameController,
-                        decoration: const InputDecoration(
-                          labelText: "Username",
+              Transform.translate(
+                offset: Offset(0, -30),
+                child: Form(
+                  key: _formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+                        child: TextFormField(
+                          controller: _usernameController,
+                          decoration: const InputDecoration(
+                            labelText: "Username",
+                          ),
+                          validator: _emailValidator,
+                          style: const TextStyle(fontSize: 20),
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "Enter an email";
-                          }
-                          return null;
-                        },
-                        style: const TextStyle(fontSize: 20),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
-                      child: TextFormField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: "Password",
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+                        child: TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: "Password",
+                          ),
+                          validator: _passwordValidator,
+                          style: const TextStyle(fontSize: 20),
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "Enter a password";
-                          }
-                          return null;
-                        },
-                        style: const TextStyle(fontSize: 20),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                      child: ElevatedButton(
-                        onPressed: _register,
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(48),
-                          backgroundColor: const Color.fromRGBO(12, 11, 61, 1),
-                          foregroundColor: Colors.white,
+                      Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                          child: ElevatedButton(
+                            onPressed: _register,
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(48),
+                              backgroundColor:
+                                  const Color.fromRGBO(12, 11, 61, 1),
+                              foregroundColor: Colors.white,
+                            ),
+                            child: Text("Login",
+                                style: GoogleFonts.poppins(fontSize: 24)),
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 40),
+                        child: Wrap(
+                          spacing: 0.8,
+                          alignment: WrapAlignment.center,
+                          children: [
+                            Text("Do you already have an account?",
+                                style: GoogleFonts.poppins(fontSize: 16)),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => LoginPage()));
+                                },
+                                style: TextButton.styleFrom(
+                                  minimumSize: const Size.fromHeight(40),
+                                ),
+                                child: Text("Login",
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 16, color: Colors.blue)))
+                          ],
                         ),
-                        child: Text("Login", style: GoogleFonts.poppins( fontSize: 24 )),
-                      )
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 40),
-                      child: Wrap(
-                        spacing: 0.8,
-                        alignment: WrapAlignment.center,
-                        children: [
-                          Text("Do you already have an account?", style: GoogleFonts.poppins( fontSize: 16 )),
-                          TextButton(
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                    MaterialPageRoute(builder: (context) => LoginPage())
-                                );
-                              },
-                              style: TextButton.styleFrom(
-                                minimumSize: const Size.fromHeight(40),
-                              ),
-                              child: Text("Login", style: GoogleFonts.poppins( fontSize: 16, color: Colors.blue ))
-                          )
-                        ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-
-          ],
-        ),
-      )
-    );
+            ],
+          ),
+        ));
   }
-
 }
